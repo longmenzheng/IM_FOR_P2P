dd37aae070c9a013b8f7227d23d3266bd2051728
lmily456 <383897248@qq.com>
2020-07-31 11:08:45 +0800

    王飞提交测试

--- client/IM_FOR_P2P/main.cpp
+++ client/IM_FOR_P2P/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //MainWindow w;
-    //w.show();
+    //w.show(); test
     ClientManager clientManager;
     clientManager.run();
 
